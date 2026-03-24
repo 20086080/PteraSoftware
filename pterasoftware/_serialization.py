74c02d894927e91c197ee96a4c7a2be22907d931
camUrban <camerongurban@gmail.com>
2026-03-24 14:10:47 -0400

    Add serialization module scaffold

---
+++ pterasoftware/_serialization.py
@@ -0,0 +1,7 @@
+"""Contains the functions for serializing and deserializing Ptera Software objects."""
+
+from __future__ import annotations
+
+from . import _logging
+
+_logger = _logging.get_logger("_serialization")
